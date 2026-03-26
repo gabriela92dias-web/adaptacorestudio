4b86889cd7e0e7799e5760df6358cef9ccdd612c
gabriela92dias-web <gabriela92dias@gmail.com>
2026-03-26 16:41:15 -0300

    Fix: Add global UserRoute guard to AppLayout

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { AppSidebar } from "./AppSidebar";
 import { useDragScroll } from "../helpers/useDragScroll";
+import { UserRoute } from "./ProtectedRoute";
 import styles from "./AppLayout.module.css";
 
 export const AppLayout = ({ children }: { children: React.ReactNode }) => {
   const { ref, isGrabbing, isSpacePressed } = useDragScroll<HTMLElement>();
 
   return (
-    <div className={styles.container}>
-      <AppSidebar />
-      <main 
-        ref={ref} 
-        className={`${styles.mainContent} ${isGrabbing ? styles.isGrabbing : ""} ${isSpacePressed && !isGrabbing ? styles.spaceHeld : ""}`}
-      >
-        <div className={styles.contentWrapper}>{children}</div>
-      </main>
-    </div>
+    <UserRoute>
+      <div className={styles.container}>
+        <AppSidebar />
+        <main 
+          ref={ref} 
+          className={`${styles.mainContent} ${isGrabbing ? styles.isGrabbing : ""} ${isSpacePressed && !isGrabbing ? styles.spaceHeld : ""}`}
+        >
+          <div className={styles.contentWrapper}>{children}</div>
+        </main>
+      </div>
+    </UserRoute>
   );
 };
