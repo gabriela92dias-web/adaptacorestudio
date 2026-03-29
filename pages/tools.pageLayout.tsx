b9a1b1801980eb8f4ebce40d8b4b0c4206fb235e
gabriela92dias-web <gabriela92dias@gmail.com>
2026-03-29 14:15:38 -0300

    fix(tools): revert double AppLayout causing broken UI in tools subpages

--- pages/tools.pageLayout.tsx
+++ pages/tools.pageLayout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { UserRoute } from "../components/ProtectedRoute";
-import { AppLayout } from "../components/AppLayout";
 
 export default [
-  ({ children }: { children: React.ReactNode }) => <UserRoute><AppLayout>{children}</AppLayout></UserRoute>
+  ({ children }: { children: React.ReactNode }) => <UserRoute>{children}</UserRoute>
 ];
